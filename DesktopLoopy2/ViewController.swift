34efca6df14f6ec2f58bb0ae82d7dbc6e06559cd
AndyWW214434 <zeanww@berkeley.edu>
2025-05-19 17:53:04 -0700

    Initial Commit

---
+++ DesktopLoopy2/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  DesktopLoopy2
+//
+//  Created by Andy Wang on 5/19/25.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ DesktopLoopy2Tests/DesktopLoopy2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  DesktopLoopy2Tests.swift
+//  DesktopLoopy2Tests
+//
+//  Created by Andy Wang on 5/19/25.
+//
+
+import Testing
+@testable import DesktopLoopy2
+
+struct DesktopLoopy2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
